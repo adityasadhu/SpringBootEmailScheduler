21fce593249c3e523d77ac9fac95aa1bf0f1336c
aditya sadhu <aditya.hf96@gmail.com>
2018-11-02 12:49:34 -0400

    Initial Working Application

---
+++ src/main/java/com/aditya/emailScheduling/quartzdemo/job/EmailJob.java
@@ -0,0 +1,62 @@
+package com.aditya.emailScheduling.quartzdemo.job;
+
+
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class EmailJob extends QuartzJobBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailJob.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private MailProperties mailProperties;
+
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        logger.info("Executing Job with key {}", jobExecutionContext.getJobDetail().getKey());
+
+        JobDataMap jobDataMap = jobExecutionContext.getMergedJobDataMap();
+        String subject = jobDataMap.getString("subject");
+        String body = jobDataMap.getString("body");
+        String recipientEmail = jobDataMap.getString("email");
+
+        sendMail(mailProperties.getUsername(), recipientEmail, subject, body);
+    }
+
+
+    private void sendMail(String fromEmail, String toEmail, String subject, String body){
+        try {
+            logger.info("Sending Email to {}", toEmail);
+            MimeMessage message = javaMailSender.createMimeMessage();
+
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, StandardCharsets.UTF_8.toString());
+            messageHelper.setFrom(fromEmail);
+            messageHelper.setSubject(subject);
+            messageHelper.setText(body, true);
+            messageHelper.setTo(toEmail);
+            javaMailSender.send(message);
+
+
+        } catch (MessagingException e) {
+            logger.error("Failed to send email to {}", toEmail);
+        }
+    }
+}
---
+++ src/main/java/com/aditya/emailScheduling/quartzdemo/payload/ScheduleEmailRequest.java
@@ -0,0 +1,67 @@
+package com.aditya.emailScheduling.quartzdemo.payload;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class ScheduleEmailRequest {
+
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String subject;
+
+    @NotEmpty
+    private String body;
+
+    @NotNull
+    private LocalDateTime dataTime;
+
+    @NotNull
+    private ZoneId timeZone;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getDataTime() {
+        return dataTime;
+    }
+
+    public void setDataTime(LocalDateTime dataTime) {
+        this.dataTime = dataTime;
+    }
+
+    public ZoneId getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(ZoneId timeZone) {
+        this.timeZone = timeZone;
+    }
+}
